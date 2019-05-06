c9858dc5d53ff70729a749b98cf7f281a5429463
msweeneydev <mail@msweeneydev.com>
2019-05-06 23:03:45 +0100

    change example content

--- charge/source/index.html.jsx
+++ charge/source/index.html.jsx
@@ -1,9 +1,9 @@
-import About from "./pages/about.html.mdx";
+import About from './pages/about.html.mdx';
 
 export default () => {
   return (
     <>
-      <h1>Charge.js Deployed on ZEIT Now</h1>
+      <h1>Welcome to my new Charge site!</h1>
       <About />
     </>
   );
